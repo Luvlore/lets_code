add829a37a5514bf4b66d2082d639c3db0e1f857
Bruna Luchini <brunaluchini22@gmail.com>
2022-02-15 09:24:24 -0300

    alteração na comparação

--- logica-da-programacao/comparador/script.js
+++ logica-da-programacao/comparador/script.js
@@ -1,12 +1,44 @@
+let result = document.getElementById('result');
 let button = document.getElementById('button');
-button.addEventListener('click', function() {
+let selectElem = document.getElementById('select');
+let selectValue = selectElem.value;
+
+selectElem.addEventListener('change', function() {
+    let value = selectElem.value;
+    selectValue = value;
+})
+
+button.addEventListener('click', function() {  
     let num1 = Number(document.getElementById('first-number').value);
     let num2 = Number(document.getElementById('second-number').value);
-    let result = document.getElementById('result');
+
+    switch (selectValue) {
+        case 'igual':
+            result.value = num1 === num2;
+            break
+            
+        case 'maior':
+            result.value = num1 > num2;
+            break
+
+        case 'maior-igual':
+            result.value = num1 >= num2;
+            break
+
+        case 'menor':
+            result.value = num1 < num2;
+            break
+
+        case 'menor-igual':
+            result.value = num1 <= num2;
+            break    
     
-    if (num1 > num2) {
-        result.value = true;
-    } else {
-        result.value = false;
+        case 'diferente':
+            result.value = num1 !== num2;;
+            break
     };
-});
+})
+
+
+
+
