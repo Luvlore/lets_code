4acff789b1dee490023500c2a7dcc832aa62731d
Bruna Luchini <brunaluchini22@gmail.com>
2022-02-16 22:22:51 -0300

    organização de arquivos e novo desafio

---
+++ logica-da-programacao/desafios/faixa-etaria/script.js
@@ -0,0 +1,22 @@
+const ageElement = document.getElementById('age');
+const button = document.getElementById('button');
+
+const result = document.getElementById('result')
+
+button.onclick = function() {
+  const ageValue = Number(ageElement.value);
+  
+  if (ageValue < 13 && ageValue > 0) {
+    result.value = 'Você é uma criança.';
+  } else if (ageValue >= 13 && ageValue < 18) {
+    result.value = 'Você é um adolescente.';
+  } else if (ageValue >= 18 && ageValue < 65) {
+    result.value = 'Você é um adulto.';
+  } else if (isNaN(ageValue) || ageValue < 0) {
+    result.value = 'Digite um valor válido!';
+  } else {
+    result.value = 'Você é um idoso.';
+  }
+
+  ageElement.value = '';
+}
